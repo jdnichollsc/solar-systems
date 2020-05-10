0fffdab8a00486e5b4a2fb4acf88cc397261c93d
Juan David Nicholls Cardona <jdnichollsc@hotmail.com>
2020-05-10 18:04:23 -0500

    Create utils.js

---
+++ utils.js
@@ -0,0 +1,32 @@
+/* ----------------------- UTILITIES ----------------------- */
+
+export function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) { /* Firefox */
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE/Edge */
+    document.msExitFullscreen();
+  }
+}
+export function toggleFullscreen() {
+  if (
+    document.fullscreenElement || 
+    document.mozFullScreenElement || 
+    document.webkitFullscreenElement || 
+    document.msFullscreenElement 
+  ) {
+    closeFullscreen();
+  } else {
+    const element = document.documentElement;
+    if (element.requestFullScreen) {
+      element.requestFullScreen();
+    } else if (element.webkitRequestFullScreen) {
+      element.webkitRequestFullScreen();
+    } else if (element.mozRequestFullScreen) {
+      element.mozRequestFullScreen();
+    }
+  }
+}
